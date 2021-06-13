259c3db37ab37952352c1affb31c5b2a776dd7f1
Riley Baxter <rbrileybaxter@gmail.com>
2021-06-13 16:56:21 -0400

    Server configuration setup, remove startup message, cache last 500 messages for each channel

--- src/widgets/audit-log/handlers/ready.js
+++ src/widgets/audit-log/handlers/ready.js
@@ -8,12 +8,36 @@ const send = require("../util/send");
 module.exports = async (client) => {
   console.log("auditLog: ready");
 
-  const builder = new AuditLogEmbedBuilder()
-    .setColor(base)
-    .setFooter("Made with ❤ by peterthehan");
-
   Object.keys(guildChannelMap)
     .map((guildId) => client.guilds.resolve(guildId))
     .filter(Boolean)
-    .forEach((guild) => send(guild, builder));
+    .forEach( async (guild) => { 
+        guild.channels.cache.forEach( async (channel) => {
+            if (channel.type != 'text') {
+                return
+            }
+            limit = 500
+            const sum_messages = [];
+            let last_id;
+            
+            while (true) {
+                const options = { limit: 100 };
+                if (last_id) {
+                    options.before = last_id;
+                }
+
+                const messages = await channel.messages.fetch(options);
+                sum_messages.push(...messages.array());
+                if (!messages.last()){
+                    return
+                }
+                last_id = messages.last().id;
+
+                if (messages.size != 100 || sum_messages >= limit) {
+                    console.log(`Fetched ${sum_messages.length} messages from ${channel.name} of ${guild.name}`)
+                    break;
+                }
+            }
+        });
+    });
 };
