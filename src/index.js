cf92881a5d462eab5b212bdb6aedec1be293db6b
Riley Baxter <rbrileybaxter@gmail.com>
2021-06-14 22:48:07 -0400

    Add auto-shutdown at 4am for Heroku

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ require("./core/loadWidgetListeners")(client);
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
+// Auto-shutdown at 4am so Heroku has less daytime restarts
+var now = new Date();
+var millisTill4 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 4, 0, 0, 0) - now;
+if (millisTill4 < 0) {
+    millisTill4 += 86400000; // After 4am, get time to 4am tomorrow
+}
+setTimeout(function() {
+    console.log('Automatic 4am shut down')
+    client.destroy()
+    process.exit()
+}, millisTill4);
+
 process.on('SIGINT', function() {
     console.log('Shutting down');
     client.destroy();
