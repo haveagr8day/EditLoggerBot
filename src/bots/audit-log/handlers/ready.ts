1890c4fc0f93a28bb580b5e72bbec3ca1c40c202
Riley Baxter <rbrileybaxter@gmail.com>
2021-10-02 00:02:18 -0400

    Fix shutdown stuff

--- src/bots/audit-log/handlers/ready.ts
+++ src/bots/audit-log/handlers/ready.ts
@@ -63,17 +63,17 @@ module.exports = async (client: Client): Promise<void> => {
     });
 };
 
-// Auto-shutdown at 4am so Heroku has less daytime restarts
+// Auto-shutdown at 8am UTC so Heroku has less daytime restarts
 let now: Date = new Date();
-let millisTill4: number = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 4, 0, 0, 0).getTime() - now.getTime();
-if (millisTill4 < 0) {
-    millisTill4 += 86400000; // After 4am, get time to 4am tomorrow
+let millisTillRestart: number = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 0, 0, 0).getTime() - now.getTime();
+if (millisTillRestart < 0) {
+    millisTillRestart += 86400000; // After 8am, get time to 8am tomorrow
 }
 setTimeout(function() {
-    console.log('Automatic 4am shut down')
+    console.log('Automatic 8am UTC shut down')
     global_client.destroy()
     process.exit()
-}, millisTill4);
+}, millisTillRestart);
 
 process.on('SIGINT', function() {
     console.log('Shutting down');
